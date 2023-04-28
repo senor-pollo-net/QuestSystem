3c5dc3163020cfc701865a352e4fbdca2d576df4
Drachenkaiser200 <80461276+Drachenkaiser200@users.noreply.github.com>
2023-04-28 21:44:28 +0200

    Update QS.java

--- src/main/java/main/QS.java
+++ src/main/java/main/QS.java
@@ -1,11 +1,23 @@
 package main;
 
+import quest.QuestManager;
+import util.Config;
+
 public class QS {
 	
 	private final Main main;
 	
+	public QuestManager questManager;
+	
+	public Config questsYML;
+	
+	public Config playersYML;
+	
+	public static QS get;
+	
 	public QS(Main main) {
 		this.main = main;
+		QS.get = main.qs;
 	}
 	
 	public Main getMain() {
@@ -13,7 +25,12 @@ public class QS {
 	}
 
 	public void onEnable() {
+		questManager = new QuestManager();
 		
+		questsYML = new Config("quests.yml");
+		playersYML = new Config("players.yml");
+		
+		questManager.load();
 	}
 
 	public void onDisable() {
